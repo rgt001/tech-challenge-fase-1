93b2127a5cb5f755f15c8c473766eb9ff963425e
rgt001 <rgt001@users.noreply.github.com>
2026-08-02 16:31:32 -0300

    Remove PDFs, DOCX and BAT from repository

--- rodar_projeto.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Roda o projeto INTEIRO (testes + treino + previsao + radiomica).
-chcp 65001 >nul
-cd /d "%~dp0"
-"D:\Projetos\FIAP\.venv\Scripts\python.exe" "%~dp0executar_projeto.py" %*
-echo.
-pause
